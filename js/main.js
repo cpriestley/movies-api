e7f64cc110458ef8cc738a2a01d113ebb9c0271e
Christina Antonuccio <antonuccio.christina@gmail.com>
2023-02-03 12:28:52 -0600

    got the edit a movie working, small visual tweaks on the front end, disabled buttons while ajax is running

--- js/main.js
+++ js/main.js
@@ -44,12 +44,12 @@ $(function() {
 
         data.forEach(function (value) {
             card += `<li className="card">`
-            card += `<div class="card" id="${value.id}" style="width: 15rem; ">`;
+            card += `<div class="card pt-3" id="${value.id}">`;
             card += `<img src="${value.Poster}" class="card-img-top" alt="${value.Title}">`;
             card += `<div class="card-body">`;
-            card += `<h6 class="card-title">${value.Title}</h6>`;
-            card += `<p class="card-text">${value.Genre}</p>`;
-            card += `<div class="d-flex flex-row justify-content-around">
+            card += `<h6 class="card-title fw-bold">${value.Title}</h6>`;
+            card += `<p class="card-text fs-6">${value.Genre}</p>`;
+            card += `<div class="d-flex flex-row justify-content-around w-80">
             <p class="rating d-flex flex-row">${value.imdbRating}</p>
             <button type="button" class="edit-btn btn btn-dark" data-bs-toggle="modal" data-bs-target="#editModal"><i class="fa-solid fa-pencil"></i></button>
             <button class="delete-btn btn btn-dark" type="submit"><i class="fa-solid fa-trash-can"></i></button>
@@ -63,6 +63,7 @@ $(function() {
         $('#movie-content').html(card);
 
     }
+
 
     //delete movies
     $('#movie-content').on("click", "button.delete-btn", function(e) {
@@ -80,23 +81,77 @@ $(function() {
     });
 
     //edit movies
-    /*$('#edit-btn').on("click", "button.edit-btn", function(e) {
+    //open modal and prefill
+    $('#movie-content').on("click", "button.edit-btn", function(e) {
+        let card = $(e.currentTarget).parent().parent().parent();
+        //link
+        let postLink = card.children('img').attr('src');
+        $("#editPoster").val(postLink);
+        //title
+        let title = card.children().first().next().children().first().text();
+        $("#editTitle").val(title);
+        //genres
+        let genres = card.children().first().next().children().first().next().text();
+        $("#editGenre").val(genres);
+        //rating
+        let rating = card.children().first().next().children().first().next().next().children().first().text();
+        $("#editImdbRating").val(rating);
+        let id = $(this).parent().parent().parent().attr('id');
+        $("#editId").val(id);
 
-    });*/
+    });
+
+    //disable edit submit button while ajax is running
+    $(document).ajaxStart(function () {
+        $("#submitEdit").attr("disabled", true);
+    });
+    $(document).ajaxComplete(function () {
+        $("#submitEdit").attr("disabled", false);
+    });
 
 
-    //under submit for edit
-    /*let id = $(this).parent().parent().attr('id');
-    console.log(MOVIES_URL + "/" + id);
-    $.ajax({
-        url: MOVIES_URL + "/" + id,
-        type: 'PATCH',
-        success: function() {
-            // Do something with the result
-            console.log("Movie deleted")
-            $('#' + id).remove();
-        }
-    });*/
+    $('#submitEdit').click(function() {
+        let title = document.getElementById("editTitle").value;
+        let rating = document.getElementById("editImdbRating").value;
+        let genre = document.getElementById("editGenre").value;
+        let poster = document.getElementById("editPoster").value;
+        let id = document.getElementById("editId").value;
+        console.log(title);
+
+        const editMovie = {
+            Title: title,
+            imdbRating: rating,
+            Genre: genre,
+            Poster: poster,
+            id: id
+        };
+        console.log(editMovie);
+
+        //under submit for edit
+        console.log(MOVIES_URL + "/" + id);
+        $.ajax({
+            url: MOVIES_URL + "/" + id,
+            type: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify(editMovie)
+        }).done(function(){
+            console.log("Movie edited")
+            getMovies();
+            /*let element = $("#editLabel");
+            element.text(element.text().replace("Edit A Movie", "Movie Has Been Edited"));*/
+        });
+
+    });
+
+    //disable add button while ajax is running
+    $(document).ajaxStart(function () {
+        $("#submitAdd").attr("disabled", true);
+    });
+    $(document).ajaxComplete(function () {
+        $("#submitAdd").attr("disabled", false);
+    });
 
 
     //add a movie
@@ -107,20 +162,20 @@ $(function() {
         let poster = document.getElementById("addPoster").value;
         console.log(title);
 
-        const reviewObj = {
+        const newMovie = {
             Title: title,
             imdbRating: rating,
             Genre: genre,
             Poster: poster,
         };
-        console.log(reviewObj);
+        console.log(newMovie);
         $.ajax({
             url: MOVIES_URL,
             type: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            data: JSON.stringify(reviewObj)
+            data: JSON.stringify(newMovie)
         }).done(function(){
             console.log("Movie ADDED")
             getMovies();
@@ -131,7 +186,7 @@ $(function() {
 
 
 
-
+    //search omdb
     let data = {
         "i": "tt3896198",
         "apikey": OMDB_API_KEY
