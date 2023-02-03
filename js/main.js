03eb60916c4f754bd479ed8de22b5734c34d633d
Christina Antonuccio <antonuccio.christina@gmail.com>
2023-02-03 10:20:29 -0600

    add a movie, loading image, all the front end, modals

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ import {OMDB_API_KEY} from "./keys.js";
 
 $(function() {
 
-    const MOVIES_URL = "https://fantastic-fortune-syrup.glitch.me/movies";
+   /* $('#loading-image').bind('ajaxStart', function(){
+        $(this).show();
+    }).bind('ajaxStop', function(){
+        $(this).hide();
+    });*/
+
+
+    const MOVIES_URL = "https://fantastic-fortune-syrup.glitch.me/movies/";
     const OMDB_URL = "http://www.omdbapi.com/";
 
     function getMovies() {
@@ -13,6 +20,11 @@ $(function() {
                 dataType: "json",
                 success: (data) => {
                     popCards(data);
+                    setTimeout( () => {
+                        $('#movie-content').removeClass("d-none");
+                        $('#loading-image').remove();
+                    }, 1200)
+
                     //$("#insertProducts").html(productRows);
                 },
                 error: function (error) {
@@ -24,27 +36,102 @@ $(function() {
 
     getMovies();
 
+
+
     function popCards(data) {
         console.log(data);
         let card = '';
 
         data.forEach(function (value) {
-            card += `<div class="card" style="width: 18rem;">`;
+            card += `<li className="card">`
+            card += `<div class="card" id="${value.id}" style="width: 15rem; ">`;
             card += `<img src="${value.Poster}" class="card-img-top" alt="${value.Title}">`;
             card += `<div class="card-body">`;
-            card += `<h5 class="card-title">${value.Title}</h5>`;
-            card += `<p class="card-text">${value.imdbRating}</p>`;
-            card += `<p class="card-text">${value.Plot}</p>`;
-            card += `<a href="#" class="btn btn-primary"><i class="fa-solid fa-pencil"></i></a>`;
-            card += `<a href="#" class="btn btn-primary"><i class="fa-solid fa-trash-can"></i></a>`;
+            card += `<h6 class="card-title">${value.Title}</h6>`;
+            card += `<p class="card-text">${value.Genre}</p>`;
+            card += `<div class="d-flex flex-row justify-content-around">
+            <p class="rating d-flex flex-row">${value.imdbRating}</p>
+            <button type="button" class="edit-btn btn btn-dark" data-bs-toggle="modal" data-bs-target="#editModal"><i class="fa-solid fa-pencil"></i></button>
+            <button class="delete-btn btn btn-dark" type="submit"><i class="fa-solid fa-trash-can"></i></button>
+            </div>`;
             card += `</div>`;
             card += `</div>`;
+            card += `</li>`;
 
         });
 
         $('#movie-content').html(card);
 
     }
+
+    //delete movies
+    $('#movie-content').on("click", "button.delete-btn", function(e) {
+        let id = $(this).parent().parent().parent().attr('id');
+        console.log(MOVIES_URL + "/" + id);
+        $.ajax({
+            url: MOVIES_URL + "/" + id,
+            type: 'DELETE',
+            success: function() {
+                // Do something with the result
+                console.log("Movie deleted")
+                $('#' + id).parent().remove();
+            }
+        });
+    });
+
+    //edit movies
+    /*$('#edit-btn').on("click", "button.edit-btn", function(e) {
+
+    });*/
+
+
+    //under submit for edit
+    /*let id = $(this).parent().parent().attr('id');
+    console.log(MOVIES_URL + "/" + id);
+    $.ajax({
+        url: MOVIES_URL + "/" + id,
+        type: 'PATCH',
+        success: function() {
+            // Do something with the result
+            console.log("Movie deleted")
+            $('#' + id).remove();
+        }
+    });*/
+
+
+    //add a movie
+    $('#submitAdd').click(function() {
+        let title = document.getElementById("addTitle").value;
+        let rating = document.getElementById("addImdbRating").value;
+        let genre = document.getElementById("addGenre").value;
+        let poster = document.getElementById("addPoster").value;
+        console.log(title);
+
+        const reviewObj = {
+            Title: title,
+            imdbRating: rating,
+            Genre: genre,
+            Poster: poster,
+        };
+        console.log(reviewObj);
+        $.ajax({
+            url: MOVIES_URL,
+            type: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify(reviewObj)
+        }).done(function(){
+            console.log("Movie ADDED")
+            getMovies();
+        });
+
+
+    });
+
+
+
+
     let data = {
         "i": "tt3896198",
         "apikey": OMDB_API_KEY
